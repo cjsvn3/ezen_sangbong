d5e7776b2f8b6e32331fca2cfd9b284a0d6bab26
cjsvn3 <91936928+cjsvn3@users.noreply.github.com>
2021-10-05 15:20:30 +0900

    2nd commit

--- exam.java
+++ exam.java
@@ -8,9 +8,9 @@ public class exam {
 		Scanner sc = new Scanner(System.in);
 		double weight = sc.nextDouble();
 		sc.close();
-		System.out.println(weight);
-
-		if (weight <= 50.86) {
+		
+		System.out.println("pureun");
+		if (weight <= 50.80) {
 			System.out.println("Flyweight");
 		} else if (weight <= 61.23) {
 			System.out.println("Lightweight");
