6ee998b421af0e0ff66fe43f119c3de1f9e3f98d
cjsvn3 <91936928+cjsvn3@users.noreply.github.com>
2021-10-05 15:17:52 +0900

    Intial commit

---
+++ exam.java
@@ -0,0 +1,26 @@
+package jungol;
+
+import java.util.Scanner;
+
+public class exam {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double weight = sc.nextDouble();
+		sc.close();
+		System.out.println(weight);
+
+		if (weight <= 50.86) {
+			System.out.println("Flyweight");
+		} else if (weight <= 61.23) {
+			System.out.println("Lightweight");
+		} else if (weight <= 72.57) {
+			System.out.println("Middleweight");
+		} else if (weight <= 88.45) {
+			System.out.println("Cruiserweight");
+
+		} else{
+			System.out.println("Heavyweight");
+		}
+	}
+}
